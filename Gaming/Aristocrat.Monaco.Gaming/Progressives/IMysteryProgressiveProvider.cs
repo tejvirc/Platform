38d79d130265dfd4a414c3c29642b791391cc10d
Alex Palmer <alex.palmer@aristocrat.com>
2022-06-01 15:53:46 +1000

    Adds interface for mystery progressives

---
+++ Gaming/Aristocrat.Monaco.Gaming/Progressives/IMysteryProgressiveProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace Aristocrat.Monaco.Gaming.Progressives
+{
+    using Contracts.Progressives;
+
+    /// <summary>
+    ///     Provides an interface for mystery progressives
+    /// </summary>
+    public interface IMysteryProgressiveProvider
+    {
+        /// <summary>
+        ///     Generates a valid magic number for the given progressive level and persists it to storage.
+        /// </summary>
+        /// <param name="progressiveLevel">The progressive level for which to generate the magic number</param>
+        /// <returns>The magic number generated</returns>
+        decimal GenerateMagicNumber(ProgressiveLevel progressiveLevel);
+
+        /// <summary>
+        ///     Retrieves the magic number for a progressive level
+        /// </summary>
+        /// <param name="assignedProgressiveKey">The assigned progressive key for the level</param>
+        /// <returns>The magic number for the progressive level</returns>
+        decimal GetMagicNumber(string assignedProgressiveKey);
+
+        /// <summary>
+        ///     Determines if the progressive level has met the requirements for triggering mystery jackpot
+        /// </summary>
+        /// <param name="assignedProgressiveKey">The assigned progressive key for the level</param>
+        /// <returns>True if mystery jackpot requirements are met, false otherwise</returns>
+        bool CheckMysteryJackpot(string assignedProgressiveKey);
+    }
+}
