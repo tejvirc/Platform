5b901033360efcfb7e366b0096fdd1d7db740f51
Thanh Dao <thanh.dao@aristocrat.com>
2022-06-29 13:22:32 +1000

    support fan speed calculation in RPM by the given CPU temperature, add coversion from RPM to PWM based on Gen8 characteristic

--- Hardware/Aristocrat.Monaco.Hardware.Contracts/Fan/IFan.cs
+++ Hardware/Aristocrat.Monaco.Hardware.Contracts/Fan/IFan.cs
@@ -12,15 +12,17 @@
         int GetFanSpeed();
 
         /// <summary>
-        ///     Gets fan speed method
+        ///     Calculate the fan speed for the given temperature
+        /// <parameter name ="temperature"> the current CPU temperature</parameter> 
+        /// <returns>Returns fan speed in RPMs</returns>
         /// </summary>
-        /// <returns>fan speed in rpm</returns>
-        int CalculateFanspeed(int Tempreture);
+        int CalculateFanSpeed(float temperature);
 
         /// <summary>
-        ///     Gets fan speed method
+        ///     Calculate the fan speed in PWM for the given RPM
+        /// <parameter name ="fanSpeed"> the speed in RPM</parameter> 
         /// </summary>
-        /// <returns>fan speed in rpm</returns>
-        int CalculatePWN(int Fanspeed);
+        /// <returns>Returns fan speed in PWM</returns>
+        int CalculatePwm(int fanSpeed);
     }
 }
--- Hardware/Aristocrat.Monaco.Hardware/Services/FanService.cs
+++ Hardware/Aristocrat.Monaco.Hardware/Services/FanService.cs
@@ -2,15 +2,34 @@
 {
     using System;
     using System.Collections.Generic;
-    using Aristocrat.Monaco.Hardware.Contracts.Fan;
-    using Contracts.Battery;
+    using System.Linq;
+    using Contracts.Fan;
     using Contracts.IO;
     using Kernel;
 
     public class FanService : IFan, IService
     {
         private readonly IIO _io;
-        private readonly object _lockObject = new object();
+        private readonly object _lockObject = new();
+        private const int MaxPwm = 255;
+        private const int MaxRpm = 4400;
+
+        public struct TempToSpeed
+        {
+            public uint TemperatureLow;
+            public uint TemperatureHigh;
+            public uint SpeedLow;
+            public uint SpeedHigh;
+        };
+
+        // temperature vs fan-speed threshold map for gen8 cabinet
+        public TempToSpeed[] TempToSpeedTableGen8 = {
+            new() { TemperatureLow = 80, TemperatureHigh = 100, SpeedLow = 4400, SpeedHigh = 4400 },
+            new() { TemperatureLow = 75, TemperatureHigh = 80, SpeedLow = 4000, SpeedHigh = 4480 },
+            new() { TemperatureLow = 65, TemperatureHigh = 75, SpeedLow = 2500, SpeedHigh = 4000 },
+            new() { TemperatureLow = 35, TemperatureHigh = 65, SpeedLow = 1000, SpeedHigh = 2500 },
+            new() { TemperatureLow = 0, TemperatureHigh = 34, SpeedLow = 0, SpeedHigh = 0 }
+        };
 
         public FanService()
             : this(ServiceManager.GetInstance().GetService<IIO>())
@@ -40,14 +59,17 @@
             return _io.GetFanPwm();
         }
 
-        public int CalculateFanspeed(int Tempreture)
+        public int CalculateFanSpeed(float temperature)
         {
-            return 0;
+            return (int) (from m in TempToSpeedTableGen8
+                where m.TemperatureLow <= temperature && m.TemperatureHigh >= temperature
+                    let r = (m.SpeedHigh - m.SpeedLow) / (m.TemperatureHigh - m.TemperatureLow)
+                select m.SpeedLow + Convert.ToUInt32(r * (temperature - m.TemperatureLow))).FirstOrDefault();
         }
 
-        public int CalculatePWN(int Fanspeed)
+        public int CalculatePwm(int fanSpeed)
         {
-            return 0;
+            return fanSpeed * MaxPwm / MaxRpm;
         }
     }
 }
