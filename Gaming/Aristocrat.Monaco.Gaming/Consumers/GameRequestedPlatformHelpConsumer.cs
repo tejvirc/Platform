5273a8a7fbe5c002bb75b46c26e5a09214ffc4e2
Ian-Pillow <104388341+Ian-Pillow@users.noreply.github.com>
2022-07-25 14:50:11 -0500

    TXM-7308 Pass Flag To Runtime When Platform Closes Help (#286)
    We change the InPlatformHelp flag to notify the game through the runtime when platform enters/exits help

---
+++ Gaming/Aristocrat.Monaco.Gaming/Consumers/GameRequestedPlatformHelpConsumer.cs
@@ -0,0 +1,31 @@
+﻿namespace Aristocrat.Monaco.Gaming.Consumers
+{
+    using System;
+    using Contracts;
+    using Runtime;
+    using Runtime.Client;
+
+    /// <summary>
+    ///     Handles the GameRequestedPlatformHelpEvent
+    /// </summary>
+    public class GameRequestedPlatformHelpConsumer : Consumes<GameRequestedPlatformHelpEvent>
+    {
+        private readonly IRuntime _runtime;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GameRequestedPlatformHelpEvent" /> class.
+        /// </summary>
+        /// <param name="runtime">An <see cref="IRuntime" /> instance.</param>
+        public GameRequestedPlatformHelpConsumer(
+            IRuntime runtime)
+        {
+            _runtime = runtime ?? throw new ArgumentNullException(nameof(runtime));
+        }
+
+        /// <inheritdoc />
+        public override void Consume(GameRequestedPlatformHelpEvent theEvent)
+        {
+            _runtime.UpdateFlag(RuntimeCondition.InPlatformHelp, theEvent.Visible);
+        }
+    }
+}
