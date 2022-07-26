20da9e08a2171e72d99fe0a782befb1f97ba8689
DeshawnRivers94 <85764030+DeshawnRivers94@users.noreply.github.com>
2022-07-26 14:39:17 -0500

    TXM-7125 Removing the GEW message on cashout and at the start of game (#285)
    TXM-7125 Removing the GEW message on cashout and at the start of a new game

--- Protocol/Bingo/Aristocrat.Monaco.Bingo/GameEndWin/GameEndWinBonusCreditsStrategy.cs
+++ Protocol/Bingo/Aristocrat.Monaco.Bingo/GameEndWin/GameEndWinBonusCreditsStrategy.cs
@@ -1,15 +1,17 @@
 ﻿namespace Aristocrat.Monaco.Bingo.GameEndWin
 {
-    using Application.Contracts.Extensions;
-    using Application.Contracts.Localization;
-    using Gaming.Contracts.Bonus;
-    using Kernel;
-    using Localization.Properties;
     using System;
     using System.Linq;
     using System.Reflection;
     using System.Threading;
     using System.Threading.Tasks;
+    using Application.Contracts.Extensions;
+    using Application.Contracts.Localization;
+    using Gaming.Contracts;
+    using Gaming.Contracts.Bonus;
+    using Gaming.Contracts.Central;
+    using Kernel;
+    using Localization.Properties;
     using log4net;
 
     public class GameEndWinBonusCreditsStrategy : IGameEndWinStrategy
@@ -19,11 +21,25 @@
 
         private readonly IEventBus _bus;
         private readonly IBonusHandler _bonus;
+        private readonly IMessageDisplay _messages;
+        private readonly ICentralProvider _centralProvider;
+        private readonly IGameHistory _history;
 
-        public GameEndWinBonusCreditsStrategy(IEventBus bus, IBonusHandler bonus)
+        public GameEndWinBonusCreditsStrategy(
+            IEventBus bus,
+            IBonusHandler bonus,
+            IMessageDisplay messages,
+            ICentralProvider centralProvider,
+            IGameHistory history)
         {
             _bus = bus ?? throw new ArgumentNullException(nameof(bus));
             _bonus = bonus ?? throw new ArgumentNullException(nameof(bonus));
+            _messages = messages ?? throw new ArgumentNullException(nameof(messages));
+            _centralProvider = centralProvider ?? throw new ArgumentNullException(nameof(centralProvider));
+            _history = history ?? throw new ArgumentNullException(nameof(history));
+
+            _bus.Subscribe<CashOutStartedEvent>(this, _ => RemoveGewMessage());
+            _bus.Subscribe<GamePlayInitiatedEvent>(this, _ => RemoveGewMessage());
         }
 
         public async Task<bool> ProcessWin(long winAmount, CancellationToken token)
@@ -106,6 +122,32 @@
             {
                 _bus.UnsubscribeAll(this);
             }
+        }
+
+        private void RemoveGewMessage()
+        {
+            var transaction = _centralProvider.Transactions.OrderByDescending(x => x.TransactionId)
+                .FirstOrDefault(x => x.OutcomeState == OutcomeState.Acknowledged || x.OutcomeState == OutcomeState.Committed);
+            if (transaction == null)
+            {
+                return;
+            }
+
+            var playedGame = _history.GetGameHistory()
+                .FirstOrDefault(h => transaction.AssociatedTransactions.Contains(h.TransactionId));
+
+            var gameEndWinMessage = Localizer.For(CultureFor.Player).FormatString(ResourceKeys.GameEndWinAward,
+                playedGame?.GameWinBonus.CentsToDollars().FormattedCurrencyString());
+            if (playedGame?.GameWinBonus == 0)
+            {
+                return;
+            }
+
+            _messages.RemoveMessage(new DisplayableMessage(
+                () => gameEndWinMessage,
+                DisplayableMessageClassification.Informative,
+                DisplayableMessagePriority.Normal,
+                typeof(BonusAwardedEvent)));
         }
     }
 }
