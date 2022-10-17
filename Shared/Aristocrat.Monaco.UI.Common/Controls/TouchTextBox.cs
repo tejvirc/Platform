edb0cd756c4db0e58eb8424f2fc7838db96b0d3d
Cheston Williams <cheston.williams@aristocrat.com>
2022-10-17 11:42:24 -0500

    TXM-8339 On screen keyboard not displaying to set hash type seed (#606)
    Change UI control to force keyboard open

---
+++ Shared/Aristocrat.Monaco.UI.Common/Controls/TouchTextBox.cs
@@ -0,0 +1,39 @@
+﻿namespace Aristocrat.Monaco.UI.Common.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using Hardware.Contracts.Touch;
+    using Kernel;
+
+    /// <summary>
+    ///     Class definition for an TouchTextBox.  Focusing on the text box will launch the Windows Touch Keyboard.
+    /// </summary>
+    public class TouchTextBox : TextBox
+    {
+        private static readonly IEventBus EventBus;
+
+        static TouchTextBox()
+        {
+            EventBus = ServiceManager.GetInstance().GetService<IEventBus>();
+        }
+
+        /// <inheritdoc cref="TextBox"/>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            GotFocus += TextBox_GotFocus;
+            LostFocus += TextBox_LostFocus;
+        }
+
+        private static void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            EventBus?.Publish(new OnscreenKeyboardOpenedEvent(true));
+        }
+
+        private static void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            EventBus?.Publish(new OnscreenKeyboardClosedEvent(true));
+        }
+    }
+}
