6562eb7ae3be97023cece6524f78f12fe73af250
Chris Tisdale <christopher.tisdale@aristocrat.com>
2022-10-25 08:52:06 -0500

    TXM-8784 Updating CefSharp adding more default handlers for the browser (#662)
    This updates to the latest version of CefSharp and fixes the issues from
    the upgrade. This also adds the default handlers for the Browers so we
    don't just crash. This also prevents drag and drops, downloads, and
    other things that should be disallowed

---
+++ Shared/Aristocrat.Monaco.UI.Common/CefHandlers/DownloadHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Aristocrat.Monaco.UI.Common.CefHandlers
+{
+    using System;
+    using CefSharp;
+
+    /// <inheritdoc />
+    [CLSCompliant(false)]
+    public class DownloadHandler : IDownloadHandler
+    {
+        /// <inheritdoc />
+        public void OnBeforeDownload(
+            IWebBrowser chromiumWebBrowser,
+            IBrowser browser,
+            DownloadItem downloadItem,
+            IBeforeDownloadCallback callback)
+        {
+        }
+
+        /// <inheritdoc />
+        public void OnDownloadUpdated(
+            IWebBrowser chromiumWebBrowser,
+            IBrowser browser,
+            DownloadItem downloadItem,
+            IDownloadItemCallback callback)
+        {
+            //Cancel the download
+            callback.Cancel();
+        }
+    }
+}
