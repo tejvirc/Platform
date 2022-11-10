0828fa5bad76144e1c82e3526c98c353f67d86d7
Stephen Comino <Stephen.Comino@aristocrat.com>
2022-11-11 08:54:59 +1100

    TXM-8680 PrinterOverrides.xml failed in XML Validation (#571)
    **Jursidictions**: NSW, WA and Macau had issues validating their
    printerOverride.xml file with the PrinterOverride.xsd.
    
    Added:
    PrinterOverridesXMLValidation.cs test, so that jurisdictional
    printerOverride.xml files are validated when tests are run.
    
    Fixed:
    PrinterOverride.xsd contained an "IsReplaceRegexString" attribute that
    was set to Required, which was making the validation fail for lots of
    other jurisdictions. IsReplaceRegexString is not Optional and the
    default is false, which is what the value was before the changes made in
    https://jerry.aristocrat.com/browse/TXM-8112

---
+++ Test/Aristocrat.Monaco.XMLValidation.Tests/PrinterOverridesXMLValidation.cs
@@ -0,0 +1,34 @@
+﻿namespace Aristocrat.Monaco.XMLValidation.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class PrinterOverridesXMLValidation
+    {
+        private static readonly string XsdFilePath = Path.GetFullPath(
+            Path.Combine(Helper.StartupPath, @"..\..\..\..\Hardware\Aristocrat.Monaco.Hardware.Serial\Printer\PrinterOverrides.xsd"));
+
+        private string[] _directoriesList;
+        private string[] _fileList;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _directoriesList = Helper.GetDirectoriesList(Helper.XmlPathDirectory, Helper.FilterDirectoryList);
+            _fileList = Helper.GetFilesList(Helper.XmlPathDirectory, "PrinterOverrides*.xml");
+            Assert.AreEqual(_directoriesList.Length, _fileList.Length);
+        }
+
+        [TestMethod]
+        public void ValidatPrinterOverrideXmlForEachJurisdiction()
+        {
+            Helper.ValidateXmlFilesWithXsd(_fileList, XsdFilePath);
+        }
+    }
+}
