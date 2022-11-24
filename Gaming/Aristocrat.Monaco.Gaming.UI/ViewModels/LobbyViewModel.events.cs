1348e1a5237e4093e61f8e9e1c646edac79fb411
kumarde85 <88065354+kumarde85@users.noreply.github.com>
2022-11-23 16:46:16 -0800

    TXM-9190 WWR2.5 Regression: Pin Window remain open when we perform cashin (#758)
    …shin
    
    Close the player pop up menu if cashin/Cashout happen.

--- Gaming/Aristocrat.Monaco.Gaming.UI/ViewModels/LobbyViewModel.events.cs
+++ Gaming/Aristocrat.Monaco.Gaming.UI/ViewModels/LobbyViewModel.events.cs
@@ -464,6 +464,12 @@
             MvvmHelper.ExecuteOnUI(
                 () =>
                 {
+                    if (PlayerMenuPopupViewModel.IsMenuVisible)
+                    {
+                        PlayerMenuPopupViewModel.IsMenuVisible = false;
+                        HandleMessageOverlayVisibility();
+                    }
+
                     Credits = OverlayMessageUtils.ToCredits(platformEvent.NewBalance);
                     CheckForExitGame();
                     OnUserInteraction();
