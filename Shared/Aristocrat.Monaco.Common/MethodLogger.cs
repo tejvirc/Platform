02552d3d7b1ef6c49d411fc7a1991589ab613f29
Chris Tisdale <christopher.tisdale@aristocrat.com>
2022-11-30 07:57:46 -0600

    TXM-9246 Addressing a crash while monitoring the operator menu hours (#730) (#752)
    cherry-picked from 205be658

---
+++ Shared/Aristocrat.Monaco.Common/MethodLogger.cs
@@ -0,0 +1,13 @@
+﻿namespace Aristocrat.Monaco.Common
+{
+    using JetBrains.Annotations;
+
+    /// <summary>
+    ///     A method delegate for logging messages for <see cref="ScopedMethodTimer"/>
+    /// </summary>
+    /// <param name="format">The message for the logger to use for formatting</param>
+    /// <param name="caller">The caller for the function</param>
+    /// <param name="elapsed">The elapsed time</param>
+    [StringFormatMethod("format")]
+    public delegate void MethodLogger(string format, string caller, double elapsed);
+}
