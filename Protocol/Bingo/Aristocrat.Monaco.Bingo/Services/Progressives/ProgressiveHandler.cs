175e319d026eb9af1ae1367c940f6b03e0b5228c
Stephan Lemon <stephan.lemon@aristocrat.com>
2022-12-14 10:28:03 -0600

    Call progressive adapter on progressive update.

--- Protocol/Bingo/Aristocrat.Monaco.Bingo/Services/Progressives/ProgressiveHandler.cs
+++ Protocol/Bingo/Aristocrat.Monaco.Bingo/Services/Progressives/ProgressiveHandler.cs
@@ -1,21 +1,40 @@
 ﻿namespace Aristocrat.Monaco.Bingo.Services.Progressives
 {
     using System;
+    using System.Collections.Generic;
     using System.Reflection;
     using System.Threading;
     using System.Threading.Tasks;
+    using Application.Contracts;
     using Aristocrat.Bingo.Client.Messages.Progressives;
+    using Aristocrat.Monaco.Gaming.Contracts.Progressives.Linked;
     using Gaming.Contracts.Progressives;
     using log4net;
 
     public class ProgressiveHandler : IProgressiveHandler, IProgressiveUpdateHandler, IDisposable
     {
         private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod()!.DeclaringType);
+
+        private readonly IProtocolLinkedProgressiveAdapter _protocolLinkedProgressiveAdapter;
+
         private bool _disposed;
+
+        private Dictionary<int, int> _tempProgressiveIdMapping = new()
+        {
+            { 0, 10001 },
+            { 1, 10002 },
+            { 2, 10003 },
+        };
+
+        public ProgressiveHandler(
+            IProtocolLinkedProgressiveAdapter protocolLinkedProgressiveAdapter)
+        {
+            _protocolLinkedProgressiveAdapter = protocolLinkedProgressiveAdapter ?? throw new ArgumentNullException(nameof(protocolLinkedProgressiveAdapter));
+        }
 
         public Task<bool> ProcessProgressiveUpdate(ProgressiveUpdateMessage update, CancellationToken token)
         {
-            Logger.Debug("Received a progressive update response");
+            Logger.Debug($"Received a progressive update message, ProgLevel={update.ProgressiveLevel}, Amount={update.Amount}");
 
             if (update == null)
             {
@@ -23,6 +42,31 @@
             }
 
             token.ThrowIfCancellationRequested();
+
+            var progressiveLevels = _protocolLinkedProgressiveAdapter.ViewConfiguredProgressiveLevels();
+            foreach (var progressiveLevel in progressiveLevels)
+            {
+                // TODO broadcast level ids are 10001, 10002, 10003, but progressive system is using 0, 1, 2. How will this really work?
+                var mappedLevelId = _tempProgressiveIdMapping[progressiveLevel.LevelId];
+                if (mappedLevelId == update.ProgressiveLevel)
+                {
+                    var linkedLevel = new LinkedProgressiveLevel()
+                    {
+                        ProtocolName = ProtocolNames.Bingo,
+                        ProgressiveGroupId = progressiveLevel.ProgressivePackId,
+                        LevelId = progressiveLevel.LevelId,
+                        Amount = update.Amount
+                    };
+
+                    Logger.Debug($"UpdateLinkedProgressiveLevels ProgressiveGroupId={linkedLevel.ProgressiveGroupId}, LevelId={linkedLevel.LevelId}, Amount={linkedLevel.Amount}");
+
+                    _protocolLinkedProgressiveAdapter.UpdateLinkedProgressiveLevels(
+                        new[] { linkedLevel },
+                        ProtocolNames.Bingo);
+
+                    break;
+                }
+            }
 
             return Task.FromResult(true);
         }
