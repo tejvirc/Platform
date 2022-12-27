da13de07a8c36e285f3189c3f4fbf1d2256b08b8
Josh Kesler <Josh.Kesler@aristocrat.com>
2022-12-27 12:28:46 -0600

    TXM-9363 No error message for Barkeeper threshold values (#846)

---
+++ Gaming/Aristocrat.Monaco.Gaming.UI/Models/RewardLevelViewModel.cs
@@ -0,0 +1,58 @@
+﻿namespace Aristocrat.Monaco.Gaming.UI.Models
+{
+    using Aristocrat.Monaco.Gaming.Contracts.Barkeeper;
+    using Aristocrat.Monaco.Kernel;
+    using Aristocrat.MVVM.ViewModel;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public abstract class RewardLevelViewModel : BaseEntityViewModel
+    {
+        private bool _enabledField;
+        private long _thresholdInCents;
+        private IPropertiesManager _propertiesManager;
+
+        public string Name { get; set; }
+
+        public long ThresholdInCents
+        {
+            get => _thresholdInCents;
+            set
+            {
+                if (SetProperty(ref _thresholdInCents, value, nameof(ThresholdInCents)))
+                {
+                    ValidateThresholdInCents();
+                }
+            }
+        }
+
+        protected IPropertiesManager PropertiesManager => _propertiesManager ??= ServiceManager.GetInstance().TryGetService<IPropertiesManager>();
+
+        public ColorOptions Color { get; set; }
+
+        public BarkeeperAlertOptions Alert { get; set; }
+
+        public bool Enabled
+        {
+            get => _enabledField;
+            set => SetProperty(ref _enabledField, value, nameof(Enabled));
+        }
+
+        protected override void SetError(string propertyName, string error)
+        {
+            if (string.IsNullOrEmpty(error))
+            {
+                ClearErrors(propertyName);
+            }
+            else
+            {
+                base.SetError(propertyName, error);
+            }
+        }
+
+        public abstract bool ValidateThresholdInCents();
+    }
+}
