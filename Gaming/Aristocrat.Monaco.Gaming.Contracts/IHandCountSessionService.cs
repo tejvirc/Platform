e17cd05ef083837df269df3fb8b6946a70038d51
Patrick Xiong <patrick.xiong@aristocrat.com>
2023-01-10 10:24:54 +1100

    HandCountSessionService

---
+++ Gaming/Aristocrat.Monaco.Gaming.Contracts/IHandCountSessionService.cs
@@ -0,0 +1,32 @@
+﻿namespace Aristocrat.Monaco.Gaming.Contracts
+{
+    using Kernel;
+
+
+    /// <summary>
+    ///     Contract for hand count session service instance.
+    /// </summary>
+    public interface IHandCountSessionService : IService
+    {
+        /// <summary>
+        ///     Gets hand count
+        /// </summary>
+        /// <returns>GameCategorySetting</returns>
+        int HandCount { get; }
+
+        /// <summary>
+        ///     Increase hand count
+        /// </summary>
+        void IncreaseHandCount();
+
+        /// <summary>
+        ///     Decrease hand count
+        /// <param name="n">Decrease hand count by n.</param>
+        /// </summary>
+        void DecreaseHandCount(int n);
+        /// <summary>
+        ///     Reset hand count
+        /// </summary>
+        void ResetHandCount();
+    }
+}
---
+++ Gaming/Aristocrat.Monaco.Gaming/HandCountSessionService.cs
@@ -0,0 +1,72 @@
+﻿namespace Aristocrat.Monaco.Gaming
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Contracts;
+    using Kernel;
+    using log4net;
+
+    /// <summary>
+    ///     Definition of the HandCountSessionService class.
+    /// </summary>
+    public class HandCountSessionService : IHandCountSessionService
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly IPropertiesManager _properties;
+        private readonly IEventBus _eventBus;
+
+        private int _handCount = 0;
+
+        public HandCountSessionService(
+            IEventBus eventBus,
+            IPropertiesManager properties)
+        {
+            _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
+            _properties = properties ?? throw new ArgumentNullException(nameof(properties));
+        }
+
+        public string Name => typeof(GameCategoryService).FullName;
+
+        public int HandCount => _handCount;
+
+        public ICollection<Type> ServiceTypes => new[] { typeof(IHandCountSessionService) };
+
+        public void Initialize()
+        {
+            _eventBus.Subscribe<PrimaryGameStartedEvent>(this, HandleEvent);
+        }
+
+        private void HandleEvent(PrimaryGameStartedEvent evt)
+        {
+            IncreaseHandCount();
+
+        }
+
+        public void IncreaseHandCount()
+        {
+            _handCount++;
+
+            _eventBus.Publish(new HandCountChangedEvent(HandCount));
+
+            Logger.Info($"IncreaseHandCount:{HandCount}");
+        }
+
+        public void DecreaseHandCount(int n)
+        {
+            _handCount-=n;
+
+            _eventBus.Publish(new HandCountChangedEvent(HandCount));
+            Logger.Info($"DecreaseHandCount by {n} to {HandCount}");
+        }
+
+        public void ResetHandCount()
+        {
+            _handCount = 0;
+
+            _eventBus.Publish(new HandCountChangedEvent(HandCount));
+            Logger.Info($"ResetHandCount:{HandCount}");
+        }
+    }
+}
