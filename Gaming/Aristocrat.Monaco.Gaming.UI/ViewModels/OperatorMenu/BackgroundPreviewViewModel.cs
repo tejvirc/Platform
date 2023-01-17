ec512b3bf47a8e42baa54423addfe3413e8e1840
Dakotah Hicock <dakotah.hicock@aristocrat.com>
2023-01-17 13:43:49 -0600

    TXM-9840 - MarsX background preview is too large for the display (#944)

--- Gaming/Aristocrat.Monaco.Gaming.UI/ViewModels/OperatorMenu/BackgroundPreviewViewModel.cs
+++ Gaming/Aristocrat.Monaco.Gaming.UI/ViewModels/OperatorMenu/BackgroundPreviewViewModel.cs
@@ -1,22 +1,17 @@
 ﻿namespace Aristocrat.Monaco.Gaming.UI.ViewModels.OperatorMenu
 {
+    using System.Windows;
     using Application.UI.OperatorMenu;
-    using Cabinet.Contracts;
-    using Hardware.Contracts.Cabinet;
-    using Kernel;
 
     public class BackgroundPreviewViewModel : OperatorMenuSaveViewModelBase
     {
         private string _backgroundImagePath;
-
+        private readonly double _scaleFactor = 0.75;
         public BackgroundPreviewViewModel()
         {
-            var displayDevice = ServiceManager.GetInstance().GetService<ICabinetDetectionService>().GetDisplayDeviceByItsRole(DisplayRole.Main);
-
-            if (displayDevice != null)
-            {
-                BackgroundPreviewHeight = displayDevice.Bounds.Height * 0.75;
-            }
+            BackgroundPreviewHeight = SystemParameters.PrimaryScreenHeight * _scaleFactor;
+            BackgroundPreviewWidth = SystemParameters.PrimaryScreenWidth * _scaleFactor;
+            Logger.Debug($"Screen Size: {BackgroundPreviewWidth}x{BackgroundPreviewHeight}");
         }
 
         public string BackgroundImagePath
@@ -26,5 +21,6 @@
         }
 
         public double BackgroundPreviewHeight { get; }
+        public double BackgroundPreviewWidth { get; }
     }
 }
