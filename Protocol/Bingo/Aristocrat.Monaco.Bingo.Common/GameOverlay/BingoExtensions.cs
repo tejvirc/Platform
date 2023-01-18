1b9d4a86b8fa2d98705083f851f320a9cfb500f0
Chris Tisdale <christopher.tisdale@aristocrat.com>
2023-01-18 11:00:13 -0600

    TXM-9910 Fixing an issue where duplicate pattern IDs gets lost (#942) (#955)

--- Protocol/Bingo/Aristocrat.Monaco.Bingo.Common/GameOverlay/BingoExtensions.cs
+++ Protocol/Bingo/Aristocrat.Monaco.Bingo.Common/GameOverlay/BingoExtensions.cs
@@ -10,14 +10,15 @@
         /// </summary>
         /// <param name="bitPattern">The bit masked daubed pattern</param>
         /// <returns>The multi-denominational array for each space of the bingo card saying if it is daubed</returns>
-        public static bool[,] BitPatternToFlags(this int bitPattern)
+        public static bool[][] BitPatternToFlags(this int bitPattern)
         {
-            var flags = new bool[BingoConstants.BingoCardDimension, BingoConstants.BingoCardDimension];
+            var flags = new bool[BingoConstants.BingoCardDimension][];
             for (var row = BingoConstants.BingoCardDimension - 1; row >= 0; row--)
             {
+                flags[row] = new bool[BingoConstants.BingoCardDimension];
                 for (var col = BingoConstants.BingoCardDimension - 1; col >= 0; col--)
                 {
-                    flags[row, col] = (bitPattern & 1) == 1;
+                    flags[row][col] = (bitPattern & 1) == 1;
                     bitPattern >>= 1;
                 }
             }
