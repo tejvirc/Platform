3ff84d88b594490bffaef9a9294f7ba5bd90f41f
Amir Emami <amirali.emami@aristocrat.com>
2023-03-15 16:33:16 +1100

    Adds GPUinfo as a data model

---
+++ Hardware/Aristocrat.Monaco.Hardware.Contracts/Display/GpuInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace Aristocrat.Monaco.Hardware.Contracts.Display
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GpuInfo
+    {
+        public string BiosVersion { get; set; }
+
+        public string ModelName { get; set; }
+
+        public string GpuName { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public string ImageVersion { get; set; }
+
+        public uint TotalRam { get; set; }
+    }
+}
