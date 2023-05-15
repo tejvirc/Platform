feba9135fd05c891ee658af51fc5f8e8d0fda473
JoshDavisATI <josh.davis@aristocrat.com>
2023-05-15 10:59:10 -0400

    Forgot to stage PublishLinux.bat in my previous commit

--- PublishLinux.bat
+++ PublishLinux.bat
@@ -3,24 +3,27 @@ setlocal enabledelayedexpansion
 
 set CONFIG=Release
 set OUTPUT_DIR=bin\%CONFIG%\Platform\bin\linux-x64\publish
-set PUBLISH_DIR=PublishLinux
+set PUBLISH_DIR=_PublishLinux
 set ARCHIVE_NAME=PublishLinux.tar
+
+SET DEL_DIR_CMD=rmdir /S /Q
 
 echo.
 
 if exist %OUTPUT_DIR% (
     echo Deleting output dir: %OUTPUT_DIR%
-    del /F /Q %OUTPUT_DIR%\*.*
+    %DEL_DIR_CMD% %OUTPUT_DIR%
 )
 
 if exist %PUBLISH_DIR% (
     echo Deleting publish dir: %PUBLISH_DIR%
-    del /F /Q %PUBLISH_DIR%\*.*
+     %DEL_DIR_CMD% %PUBLISH_DIR%
 )
 
 echo Building...
 echo.
 dotnet publish .\Linux.sln -c Release --no-self-contained --framework net6.0 --os linux
+::dotnet publish .\Linux.sln -c Release --framework net6.0 --os linux
 
 echo.
 echo Done building
