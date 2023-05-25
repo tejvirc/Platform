b8788d6a869a25ddaaaa32d5a66f6371a83f6ce6
Ian-Pillow <Ian.Pillow@aristocrat.com>
2023-05-25 13:54:40 -0500

    added xml comment to LightShowFile

--- Hardware/Aristocrat.Monaco.Hardware.Contracts/Reel/ControlData/LightShowFile.cs
+++ Hardware/Aristocrat.Monaco.Hardware.Contracts/Reel/ControlData/LightShowFile.cs
@@ -6,27 +6,28 @@
     public class LightShowFile
     {
         /// <summary>
-        /// 
+        /// The Animation Id
         /// </summary>
         public int Id { get; set; }
 
         /// <summary>
-        /// 
+        /// The tag hash
         /// </summary>
         public int Tag { get; set; }
 
         /// <summary>
-        /// 
+        /// Number of times to loop the animation
         /// </summary>
         public byte LoopCount { get; set; }
 
         /// <summary>
-        /// 
+        /// The reel to apply the animation to
         /// </summary>
         public byte ReelIndex { get; set; }
 
         /// <summary>
-        /// 
+        /// The step at which to play the animation.
+        /// Use -1 if this does not apply
         /// </summary>
         public short Step { get; set; }
     }
