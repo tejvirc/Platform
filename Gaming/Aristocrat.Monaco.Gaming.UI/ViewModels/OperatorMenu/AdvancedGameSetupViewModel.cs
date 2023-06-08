aec3f5e2ec667d54d3085d110fa9c5520de91717
Chris Dworetzky <christopher.dworetzky@aristocrat.com>
2023-06-08 17:57:29 -0500

    TXM-12064 Don't lock levels if we are switching restrictions in AdvancedGameSetupViewModel (#1686)

--- Gaming/Aristocrat.Monaco.Gaming.UI/ViewModels/OperatorMenu/AdvancedGameSetupViewModel.cs
+++ Gaming/Aristocrat.Monaco.Gaming.UI/ViewModels/OperatorMenu/AdvancedGameSetupViewModel.cs
@@ -1082,7 +1082,11 @@
                     .GroupBy(x => x.gameId, (id, group) => (id, group.Select(x => x.config)));
                 foreach (var (gameId, configurations) in updates)
                 {
-                    updatedLevels.AddRange(SaveGameConfiguration(gameId, configurations, progressiveLevels));
+                    var levels = SaveGameConfiguration(gameId, configurations, progressiveLevels);
+                    if (!game.Value.HasRestrictionChanges())
+                    {
+                        updatedLevels.AddRange(levels);
+                    }
                 }
 
                 if (game.Value.SelectedRestriction != null)
