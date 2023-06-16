54d42d8eda28a73c38c9ffbe2db7bd550ddf2e96
Chris Dworetzky <christopher.dworetzky@aristocrat.com>
2023-06-16 09:46:08 -0500

    TXM-12197 Quebec VLT: Fixing page titles, denom selections and other localization issues in meter pages (#1760)

--- Gaming/Aristocrat.Monaco.Gaming.UI/Views/OperatorMenu/ProgressiveMetersPage.xaml.cs
+++ Gaming/Aristocrat.Monaco.Gaming.UI/Views/OperatorMenu/ProgressiveMetersPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿namespace Aristocrat.Monaco.Gaming.UI.Views.OperatorMenu
 {
+    using Aristocrat.Monaco.Application.Contracts.Localization;
+    using Aristocrat.MVVM;
+    using Aristocrat.MVVM.ViewModel;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Data;
@@ -18,17 +21,40 @@
 
         private void ProgressiveMetersPage_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
+            if (e.OldValue != null && e.OldValue is ProgressiveMetersPageViewModel oldViewModel)
+            {
+                oldViewModel.OnShouldRegenerateColumns -= ViewModel_OnCultureChange;
+            }
+
             var viewModel = (ProgressiveMetersPageViewModel) DataContext;
+            viewModel.OnShouldRegenerateColumns += ViewModel_OnCultureChange;
+            SetupMeters(viewModel);
+        }
+
+        private void ViewModel_OnCultureChange(object sender, System.EventArgs e)
+        {
+            MvvmHelper.ExecuteOnUI(() =>
+            {
+                SetupMeters(sender as ProgressiveMetersPageViewModel);
+            });
+        }
+
+        private void SetupMeters(ProgressiveMetersPageViewModel viewModel)
+        {
+            if (viewModel is not ProgressiveMetersPageViewModel)
+            {
+                return;
+            }
+
+            ProgressiveDataGrid?.Columns.Clear();
             foreach (var meterNode in viewModel.MeterNodes)
             {
                 DataGridTextColumn textColumn = new DataGridTextColumn
                 {
-                    Binding = new Binding($"[{meterNode.DisplayName}].Value"),
+                    Binding = new Binding($"[{meterNode.Order}].Value"),
+                    Header = Localizer.For(CultureFor.Operator).GetString(meterNode.DisplayNameKey),
                     Width = DataGridLength.Auto
                 };
-
-                var locBinding = new WPFLocalizeExtension.Extensions.LocTextExtension(meterNode.DisplayNameKey);
-                locBinding.SetBinding(textColumn, textColumn.GetType().GetProperty("Header"));
 
                 ProgressiveDataGrid?.Columns.Add(textColumn);
             }
