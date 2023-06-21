131203fb050bbf21729b2a8ce856f5113a5be5f4
Matt Roland <matt.roland@aristocrat.com>
2023-06-21 14:28:54 -0500

    Was able to add the new functionality to the helpers to reference the LoadGameOpeations.cs
    - Matt Roland

--- Test/automation/Aristocrat.Monaco.RobotController/Helpers/ServicesHelpers.cs
+++ Test/automation/Aristocrat.Monaco.RobotController/Helpers/ServicesHelpers.cs
@@ -55,6 +55,7 @@
             container.Register<BalanceOperations>(Lifestyle.Transient);
             container.Register<RebootRequestOperations>(Lifestyle.Transient);
             container.Register<AuditMenuOperations>(Lifestyle.Transient);
+            container.Register<LoadGameOperations>(Lifestyle.Transient);           
 
             return container;
         }
