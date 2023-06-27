027f1bb053b13b15898595286bcc40503670df93
Chris Tisdale <christopher.tisdale@aristocrat.com>
2023-06-27 10:25:17 -0500

    TXARCH-1171 Fixing bingo being broke from the merge (#1858)

--- Protocol/Bingo/Aristocrat.Monaco.Bingo.Common/Storage/Model/BingoContext.cs
+++ Protocol/Bingo/Aristocrat.Monaco.Bingo.Common/Storage/Model/BingoContext.cs
@@ -1,41 +1,26 @@
 ﻿namespace Aristocrat.Monaco.Bingo.Common.Storage.Model
 {
     using Microsoft.EntityFrameworkCore;
-    using Monaco.Common.Storage;
     using Protocol.Common.Storage;
-    using System;
-    using System.IO;
 
     public class BingoContext : DbContext
     {
         private readonly string _connectionString;
+
+        /// <summary>
+        ///     Gets a set of <see cref="Certificate"/> items.
+        /// </summary>
+        public DbSet<Certificate> Certificates { get; set; }
 
         public BingoContext(IConnectionStringResolver connectionStringResolver)
         {
             _connectionString = connectionStringResolver.Resolve();
         }
 
-        /// <summary>
-        ///     Gets a set of <see cref="BingoServerSettingsModel"/> items.
-        /// </summary>
-        public DbSet<BingoServerSettingsModel> BingoServerSettingsModel { get; set; }
-        public DbSet<Host> Host { get; set; }
-        public DbSet<ReportTransactionModel> ReportTransactionModel { get; set; }
-        public DbSet<ReportEventModel> ReportEventModel { get; set; }
-        public DbSet<WinResultModel> WinResultModel { get; set; }
-        public DbSet<Certificate> Certificates { get; set; }
-        public DbSet<BingoDaubsModel> BingoDaubsModel { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var sqliteFile = _connectionString.Replace("Data Source=", string.Empty, StringComparison.OrdinalIgnoreCase);
-            if (sqliteFile.EndsWith(".sqlite") && !File.Exists(sqliteFile))
-            {
-                using (var fs = File.Create(sqliteFile)) { }
-            }
-
-            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite(_connectionString);
         }
-
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
