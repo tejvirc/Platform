aeab2c98828b76814f57ff16aa6e7af0daf92fb3
AlexDromin <alex.dromin@aristocrat.com>
2023-07-31 09:23:12 +1000

    fix 0.99.0.142

--- Test/Aristocrat.Monaco.Sas.Tests/Voucher/SasTicketPrintedHandlerTests.cs
+++ Test/Aristocrat.Monaco.Sas.Tests/Voucher/SasTicketPrintedHandlerTests.cs
@@ -108,7 +108,7 @@
 
             _target.TicketPrintedAcknowledged();
 
-            Assert.IsTrue(_waiter.WaitOne(1000));
+            Assert.IsTrue(_waiter.WaitOne(3000));
 
             _exceptionHandler.Verify(m => m.RemoveException(It.IsAny<GenericExceptionBuilder>()), Times.Exactly(2));
             _exceptionHandler.Verify(m => m.RemoveHandler(GeneralExceptionCode.CashOutTicketPrinted), Times.Once);
