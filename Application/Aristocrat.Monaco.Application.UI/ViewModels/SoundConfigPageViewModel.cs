913bd954cf9f9f4c7ad3e078ba7159302b2449dd
Dakotah Hicock <dakotah.hicock@aristocrat.com>
2023-08-03 17:09:40 -0500

    TXM-13129 - If there are multiple sound files, set the first one to default (#2206)

--- Application/Aristocrat.Monaco.Application.UI/ViewModels/SoundConfigPageViewModel.cs
+++ Application/Aristocrat.Monaco.Application.UI/ViewModels/SoundConfigPageViewModel.cs
@@ -135,6 +135,10 @@
                 {
                     EventBus.Publish(new HardwareDiagnosticTestStartedEvent(HardwareDiagnosticDeviceCategory.Sound));
                     EventBus.Publish(new OperatorMenuWarningMessageEvent(""));
+                    if (TestViewModel?.SoundFiles?.Count > 0)
+                    {
+                        TestViewModel.Sound = TestViewModel.SoundFiles.First();
+                    }
                 }
 
                 SetProperty(ref _inTestMode, value);
