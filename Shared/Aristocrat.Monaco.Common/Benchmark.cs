f61035157e9c710b5ba11895c896d8f9ff2df02f
AlexDromin <alex.dromin@aristocrat.com>
2023-08-03 12:24:38 +1000

    improved Benchmark

--- Shared/Aristocrat.Monaco.Common/Benchmark.cs
+++ Shared/Aristocrat.Monaco.Common/Benchmark.cs
@@ -4,10 +4,8 @@
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Diagnostics;
-    using System.Linq;
-    using System.Runtime.CompilerServices;
     using System.Security.Cryptography;
-    using System.Text;
+    using System.Threading;
 
     /// <summary>
     /// 
@@ -17,12 +15,22 @@
         private readonly Stopwatch timer = new Stopwatch();
         private readonly string benchmarkName;
 
-        private static TimeSpan cumulatedTimeSpan = TimeSpan.Zero;
-        private static TimeSpan cumulatedCommitTimeSpan = TimeSpan.Zero;
+        private static object syncCumulatedValues = new();
+        private static long cumulatedTimeSpan = 0;
         private static int cumulatedBenchmarkCount = 0;
+
+        private static long cumulatedCommitTimeSpan = 0;
         private static int cumulatedCommitBenchmarkCount = 0;
+        private static long cumulatedCommitsAvg = 0;
+
+        private static long cumulatedDuplicateCommitsTimeSpan = 0;
+        private static int cumulatedDuplicateCommitsCount = 0;
+
+        private static int totalPendingCommits = 0;
+        private static int pendingCommitsCount = 0;
 
         private int threadId;
+        private static object syncHashCompute = new();
         private static SHA256 benchmarkSHA256 = SHA256.Create();
 
         private Dictionary<string, object> keyValuePairs = null;
@@ -53,6 +61,20 @@
         /// <param name="benchmarkName"></param>
         public Benchmark(string benchmarkName)
         {
+
+            if (benchmarkName == "Commit")
+            {
+                var pending = Interlocked.Increment(ref pendingCommitsCount);
+                var totalPending = Interlocked.Add(ref totalPendingCommits, pendingCommitsCount);
+                if (pending > 1)
+                {
+                    var totalCommits = cumulatedCommitBenchmarkCount;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"----- PENDING: [{pending}] - " +
+                        $"Thread Id=[{Environment.CurrentManagedThreadId}] - Avg=({totalPending})/({totalCommits})=[{(totalCommits > 0 ? ((double)(totalPending) / totalCommits) : totalPending):F4}] ----");
+                }
+            }
+
             threadId = Environment.CurrentManagedThreadId;
             this.benchmarkName = benchmarkName;
             timer.Start();
@@ -63,22 +85,35 @@
         /// </summary>
         public void Dispose()
         {
+            if (benchmarkName == "Commit")
+                Interlocked.Decrement(ref pendingCommitsCount);
+
             threadId = Environment.CurrentManagedThreadId;
             timer.Stop();
 
-            var elapsedTime = timer.Elapsed;
-            cumulatedTimeSpan += elapsedTime;
-            cumulatedBenchmarkCount++;
+            var elapsedTime = timer.Elapsed.Ticks;
+
+            var totalAvg = TimeSpan.Zero;
+            lock (syncCumulatedValues)
+            {
+                Interlocked.Add(ref cumulatedTimeSpan, elapsedTime);
+                Interlocked.Increment(ref cumulatedBenchmarkCount);
+                totalAvg = TimeSpan.FromTicks(cumulatedTimeSpan / cumulatedBenchmarkCount);
+            }
 
             if (benchmarkName == "Commit")
             {
-                cumulatedCommitBenchmarkCount++;
-                cumulatedCommitTimeSpan += elapsedTime;
+                //lock (syncCumulatedCommitValues)
+                //{
+                Interlocked.Add(ref cumulatedCommitTimeSpan, elapsedTime);
+                Interlocked.Increment(ref cumulatedCommitBenchmarkCount);
+                Interlocked.Exchange(ref cumulatedCommitsAvg, cumulatedCommitTimeSpan / cumulatedCommitBenchmarkCount);
+                //}
             }
 
-            if (elapsedTime > TimeSpan.FromSeconds(0.1))
+            if (elapsedTime > 1000000)
                 Console.ForegroundColor = ConsoleColor.Red;
-            else if (elapsedTime > TimeSpan.FromSeconds(0.01))
+            else if (elapsedTime > 100000)
                 Console.ForegroundColor = ConsoleColor.Yellow;
             else
                 Console.ForegroundColor = ConsoleColor.Gray;
@@ -88,22 +123,29 @@
                 if (Console.ForegroundColor == ConsoleColor.White)
                     Console.ForegroundColor = ConsoleColor.Gray;
 
-                Console.WriteLine($"Thread Id=[{Environment.CurrentManagedThreadId}] : ");
+                //Console.WriteLine($"Thread Id=[{Environment.CurrentManagedThreadId}] : ");
 
                 foreach (var kv in keyValuePairs)
                 {
-                    var hash = kv.Value is byte[]? Convert.ToHexString(benchmarkSHA256.ComputeHash((byte[])kv.Value)) : kv.Value?.ToString();
+                    var hash = string.Empty;
+                    lock (syncHashCompute)
+                    {
+                        hash = kv.Value is byte[]? Convert.ToHexString(benchmarkSHA256.ComputeHash((byte[])kv.Value)) : kv.Value?.ToString();
+                    }
                     var length = kv.Value is byte[]? ((byte[])kv.Value).Length : kv.Value?.ToString().Length;
 
-                    if (benchmarkName == "Commit" && length > 32)
+                    if (benchmarkName == "Commit" && length > 100)
                     {
                         if (commitHistoryDictionary.TryGetValue(kv.Key, out var value))
                         {
                             if (value.ContainsKey(hash))
                             {
-                                Console.BackgroundColor = ConsoleColor.Cyan;
-                                Console.WriteLine($"[{kv.Key}]=hash[{hash}][{length}];");
+                                Console.BackgroundColor = ConsoleColor.DarkCyan;
+                                Console.WriteLine($"[{Environment.CurrentManagedThreadId}][{kv.Key}]=hash[{hash}][{length}];");
                                 Console.BackgroundColor = ConsoleColor.Black;
+
+                                Interlocked.Increment(ref cumulatedDuplicateCommitsCount);
+                                Interlocked.Add(ref cumulatedDuplicateCommitsTimeSpan, elapsedTime);
 
                                 continue;
                             }
@@ -120,11 +162,13 @@
                         }
                     }
 
-                    Console.WriteLine($"[{kv.Key}]=hash[{hash}][{length}];");
+                    Console.WriteLine($"[{Environment.CurrentManagedThreadId}][{kv.Key}]=hash[{hash}][{length}];");
                 }
             }
 
-            Console.WriteLine($"[{DateTime.Now} - {benchmarkName} - {elapsedTime}];Thread Id=[{Environment.CurrentManagedThreadId}];Total=[{cumulatedTimeSpan}][{cumulatedBenchmarkCount}];Avg=[{(cumulatedBenchmarkCount > 0 ? cumulatedTimeSpan.Ticks / cumulatedBenchmarkCount : 0)}];Commit Total=[{cumulatedCommitTimeSpan}][{cumulatedCommitBenchmarkCount}];Commit Avg=[{(cumulatedCommitBenchmarkCount > 0 ? cumulatedCommitTimeSpan.Ticks / cumulatedCommitBenchmarkCount : 0)}]");
+            Console.WriteLine($"{benchmarkName} - [{Environment.CurrentManagedThreadId}] - [{DateTime.Now}] - [{elapsedTime}];Total=[{TimeSpan.FromTicks(cumulatedTimeSpan)}][{cumulatedBenchmarkCount}];" +
+                $"Avg=[{totalAvg}];Commit Total=[{TimeSpan.FromTicks(cumulatedCommitTimeSpan)}][{cumulatedCommitBenchmarkCount}];" +
+                $"Commit Avg=[{cumulatedCommitsAvg}];Duplicates Total=[{TimeSpan.FromTicks(cumulatedDuplicateCommitsTimeSpan)}][{cumulatedDuplicateCommitsCount}];");
         }
     }
 }
