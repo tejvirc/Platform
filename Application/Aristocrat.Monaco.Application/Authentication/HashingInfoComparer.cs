9c4547ea7dbde1cb7c8ee9c8280f7562f90b5c6d
Chris Tisdale <christopher.tisdale@aristocrat.com>
2023-08-04 09:50:36 -0500

    TXARCH-1310 Fixing signature check due to breaking .net5 change for l… (#2209)
    …ocalization
    
    Due to a breaking change in .net5 we have to work around the sorting
    order being changed. For information on the breaking change see
    https://learn.microsoft.com/en-us/dotnet/standard/base-types/string-comparison-net-5-plus

---
+++ Application/Aristocrat.Monaco.Application/Authentication/HashingInfoComparer.cs
@@ -0,0 +1,40 @@
+﻿namespace Aristocrat.Monaco.Application.Authentication;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class HashingInfoComparer : IComparer<string>, IEqualityComparer<string>
+{
+    public int Compare(string x, string y)
+    {
+        var leftWithout = Path.ChangeExtension(x, null);
+        var rightWithout = Path.ChangeExtension(y, null);
+        if (string.Equals(leftWithout, rightWithout, StringComparison.Ordinal))
+        {
+            return StringComparer.InvariantCulture.Compare(x, y);
+        }
+
+        if (leftWithout.Contains(rightWithout, StringComparison.InvariantCulture))
+        {
+            return 1;
+        }
+
+        if (rightWithout.Contains(leftWithout, StringComparison.InvariantCulture))
+        {
+            return -1;
+        }
+
+        return StringComparer.InvariantCulture.Compare(x, y);
+    }
+
+    public bool Equals(string x, string y)
+    {
+        return string.Equals(x, y, StringComparison.Ordinal);
+    }
+
+    public int GetHashCode(string obj)
+    {
+        return obj?.GetHashCode(StringComparison.InvariantCulture) ?? throw new ArgumentNullException(obj);
+    }
+}
---
+++ Test/Aristocrat.Monaco.Application.Tests/Authentication/HashingInfoComparerTests.cs
@@ -0,0 +1,56 @@
+﻿namespace Aristocrat.Monaco.Application.Tests.Authentication;
+
+using System.Collections.Generic;
+using System.Linq;
+using Application.Authentication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class HashingInfoComparerTests
+{
+    private static IEnumerable<object[]> TestData => new List<object[]>
+    {
+        new object[]
+        {
+            new[]
+            {
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_nsw_nocsap_forced_min.xml",
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_nsw_nocsap.xml"
+            },
+            new[]
+            {
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_nsw_nocsap.xml",
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_nsw_nocsap_forced_min.xml"
+            }
+        },
+        new object[]
+        {
+            new[]
+            {
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_qld_mars.xml",
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_qld_antebet.xml",
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_qld.xml",
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_qld_nocsap_forced_min.xml",
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_qld_nocsap.xml"
+            },
+            new[]
+            {
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_qld.xml",
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_qld_antebet.xml",
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_qld_mars.xml",
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_qld_nocsap.xml",
+                "ATI_WW_BigFortune_0.99.0.144\\bin\\configuration_qld_nocsap_forced_min.xml"
+            }
+        }
+    };
+
+    private readonly HashingInfoComparer _target = new();
+
+    [DynamicData(nameof(TestData))]
+    [DataTestMethod]
+    public void SortingTest(string[] input, string[] expected)
+    {
+        var actual = input.OrderBy(x => x, _target).ToArray();
+        CollectionAssert.AreEqual(expected, actual);
+    }
+}
