6ee10803dea62d67e218b80b2bd57bc2ab9d3745
Christina <christina.kochan@aristocrat.com>
2023-08-10 15:09:16 -0500

    TXM-13195 In game soft error messages should be in operator culture (#2288)
    https://jerry.aristocrat.com/browse/TXM-13195

--- Application/Aristocrat.Monaco.Application/ApplicationRunnable.cs
+++ Application/Aristocrat.Monaco.Application/ApplicationRunnable.cs
@@ -655,7 +655,7 @@ namespace Aristocrat.Monaco.Application
             // Create a soft error message for Power Reset
             var display = ServiceManager.GetInstance().GetService<IMessageDisplay>();
             var powerResetMessage = new DisplayableMessage(
-                () => Localizer.DynamicCulture().GetString(ResourceKeys.PowerResetText),
+                () => Localizer.For(CultureFor.Operator).GetString(ResourceKeys.PowerResetText),
                 DisplayableMessageClassification.Informative,
                 DisplayableMessagePriority.Immediate,
                 typeof(PlatformBootedEvent),
