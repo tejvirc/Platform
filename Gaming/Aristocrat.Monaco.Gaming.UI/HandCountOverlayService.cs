8bb6f069bad0d83e2db1f9897bfa3b621538ca07
NeethaKuttappa <113321748+NeethaKuttappa@users.noreply.github.com>
2023-08-16 17:23:39 +1000

    TXM-13436 GGGACOAMMC-809 - Delay in loading and wrong cash out value on the banner (#2359)
    **Note: COAM Issue merge into main**
    
    Issue: There is delay in loading the hand count value in the banner
    
    Solution: Set the dialog's visibility to hidden and once the hand count
    amount is calculated and dialog is injected to main view make the
    visibility to visible.
    
    Also set the handCountAmount property to 0 after we remove the dialog

--- Gaming/Aristocrat.Monaco.Gaming.UI/HandCountOverlayService.cs
+++ Gaming/Aristocrat.Monaco.Gaming.UI/HandCountOverlayService.cs
@@ -14,6 +14,7 @@
     using System.Reflection;
     using ViewModels;
     using Views.Overlay;
+    using System.Windows;
 
     public class HandCountOverlayService : IHandCountOverlayService, IService, IDisposable
     {
@@ -113,6 +114,7 @@
             }
 
             _eventBus.Publish(new ViewInjectionEvent(_cashoutDialog, DisplayRole.Main, ViewInjectionEvent.ViewAction.Remove));
+            _cashoutDialogViewModel.HandCountAmount = 0;
         }
 
         private void Handle(CashoutAmountAuthorizationReceivedEvent evt)
@@ -123,17 +125,27 @@
             }
 
             _eventBus.Publish(new ViewInjectionEvent(_cashoutDialog, DisplayRole.Main, ViewInjectionEvent.ViewAction.Remove));
+            _cashoutDialogViewModel.HandCountAmount = 0;
         }
 
         private void Handle(CashoutAmountAuthorizationRequestedEvent evt)
         {
-            _cashoutDialogViewModel.HandCountAmount = (long)(_handCountService.HandCount * _cashOutAmountPerHand).MillicentsToDollars();
-            if (ButtonDeckFilter is not null)
-            {
-                ButtonDeckFilter.FilterMode = ButtonDeckFilterMode.Lockup;
-            }
+            MvvmHelper.ExecuteOnUI(
+               () =>
+               {
+                   //Cashout dialog visibility is set to Hidden and then to Visible once the dialog is added to the main view
+                   //as there is a delay in rendering the handcount amount value in the dialog
+                   //Please refer to https://jerry.aristocrat.com/browse/TXM-13436
+                   _cashoutDialog.Visibility = Visibility.Hidden;
+                   _cashoutDialogViewModel.HandCountAmount = (long)(_handCountService.HandCount * _cashOutAmountPerHand).MillicentsToDollars();
+                   if (ButtonDeckFilter is not null)
+                   {
+                       ButtonDeckFilter.FilterMode = ButtonDeckFilterMode.Lockup;
+                   }
 
-            _eventBus.Publish(new ViewInjectionEvent(_cashoutDialog, DisplayRole.Main, ViewInjectionEvent.ViewAction.Add));
+                   _eventBus.Publish(new ViewInjectionEvent(_cashoutDialog, DisplayRole.Main, ViewInjectionEvent.ViewAction.Add));
+                   _cashoutDialog.Visibility = Visibility.Visible;
+               });
         }
 
         private void Handle(HandCountResetTimerCancelledEvent obj)
@@ -153,6 +165,7 @@
             ButtonDeckFilter.FilterMode = ButtonDeckFilterMode.Normal;
 
             _eventBus.Publish(new ViewInjectionEvent(_timerDialog, DisplayRole.Main, ViewInjectionEvent.ViewAction.Remove));
+            _cashoutDialogViewModel.HandCountAmount = 0;
         }
 
         private void HandCountResetTimerCancelled()
@@ -162,6 +175,7 @@
 
             _timerDialogViewModel.OnHandCountTimerCancelled();
             _eventBus.Publish(new ViewInjectionEvent(_timerDialog, DisplayRole.Main, ViewInjectionEvent.ViewAction.Remove));
+            _cashoutDialogViewModel.HandCountAmount = 0;
         }
 
         private void HandCountResetTimerStarted()
