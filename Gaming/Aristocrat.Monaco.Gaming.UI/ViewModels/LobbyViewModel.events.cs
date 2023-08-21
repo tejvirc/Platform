95e4f8632b01bb41f468b7b8972cb4f9989ce571
CoryMayberry <cory.mayberry@aristocrat.com>
2023-08-21 12:57:48 -0500

    TXM-13517 added delay to exiting game if there was an forced cashout … (#2400)
    TXM-13517 added delay to exiting game if there was an forced cashout
    from a win, addressed thread saftey issue in the device adapter

--- Gaming/Aristocrat.Monaco.Gaming.UI/ViewModels/LobbyViewModel.events.cs
+++ Gaming/Aristocrat.Monaco.Gaming.UI/ViewModels/LobbyViewModel.events.cs
@@ -15,6 +15,7 @@
     using Application.Contracts.Media;
     using Cabinet.Contracts;
     using Commands;
+    using Common;
     using Contracts;
     using Contracts.Bonus;
     using Contracts.Events;
@@ -505,11 +506,10 @@
                         _lobbyStateManager.RemoveFlagState(LobbyState.CashOutFailure);
                     }
 
-                    if (_bank.QueryBalance() == 0 && _gameState.Idle &&
-                        !_gameState.InGameRound &&
+                    if (_bank.QueryBalance() == 0 &&
                         platformEvent.OldBalance >= _properties.GetValue(AccountingConstants.MaxCreditMeter, long.MaxValue))
                     {
-                        _overlimitCashoutProcessed = true;
+                         _overlimitCashoutProcessed = true;
                     }
 
                     HandleMessageOverlayText();
@@ -1532,13 +1532,22 @@
         {
             if (_overlimitCashoutProcessed && !MessageOverlayDisplay.IsOverlayWindowVisible)
             {
-                Logger.Debug("Cashed out after going over limit. Returning player to Lobby and changing Language to default.");
-                MvvmHelper.ExecuteOnUI(() =>
+                Task.Delay(OverlimitCashoutDelayInMS).ContinueWith(_ =>
                 {
-                    IsPrimaryLanguageSelected = true;
-                    _runtime.SetRequestExitGame(_properties.GetValue(GamingConstants.RequestGameExitOnCashout, false));
-                });
-                _overlimitCashoutProcessed = false;
+                    if (_bank.QueryBalance() == 0 && _gameState.Idle && !_gameHistory.IsRecoveryNeeded)
+                    {                    
+                        Logger.Debug("Cashed out after going over limit. Returning player to Lobby and changing Language to default.");
+                        MvvmHelper.ExecuteOnUI(() =>
+                        {
+                            IsPrimaryLanguageSelected = true;
+                            _runtime.SetRequestExitGame(_properties.GetValue(GamingConstants.RequestGameExitOnCashout, false));
+                        });
+                    }
+
+                    _overlimitCashoutProcessed = false;
+                }).HandleException(e => Logger.Error($"{e} Cashed out after going over limit failed."));
+
+                return;
             }
 
             if (_playerCashoutProcessed && !MessageOverlayDisplay.IsOverlayWindowVisible)
