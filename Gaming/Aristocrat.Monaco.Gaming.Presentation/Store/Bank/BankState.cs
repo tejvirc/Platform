e496b03a1b59fc36d44b48dfc5a898c95972708e
dietrichwestbrooks <dietrich.westbrooks@aristocrat.com>
2023-08-26 12:24:35 -0500

    WIP

--- Gaming/Aristocrat.Monaco.Gaming.Presentation/Store/Bank/BankState.cs
+++ Gaming/Aristocrat.Monaco.Gaming.Presentation/Store/Bank/BankState.cs
@@ -1,8 +1,14 @@
 ﻿namespace Aristocrat.Monaco.Gaming.Presentation.Store.Bank;
 
+using Contracts.Models;
+
 public record BankState
 {
     public bool CashOutEnabled { get; init; }
 
     public double Credits { get; init; }
+
+    public bool IsCashingOut { get; init; }
+
+    public LobbyCashOutState CurrentCashOutState { get; init; }
 }
--- Gaming/Aristocrat.Monaco.Gaming.Presentation/Store/Lobby/LobbyState.cs
+++ Gaming/Aristocrat.Monaco.Gaming.Presentation/Store/Lobby/LobbyState.cs
@@ -5,17 +5,9 @@ using Gaming.Contracts.Models;
 
 public record LobbyState
 {
-    public bool IsGamesLoaded { get; init; }
-
     public string? BackgroundImagePath { get; init; }
 
     public DateTime LastUserInteractionTime { get; init; }
-
-    public double Credits { get; init; }
-
-    public bool IsCashingOut { get; init; }
-
-    public LobbyCashOutState CurrentCashOutState { get; init; }
 
     public bool IsVoucherNotificationActive { get; init; }
 
