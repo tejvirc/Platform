5d88b04de7d16378607a9deddd9995d61283ed01
Christina <christina.kochan@aristocrat.com>
2023-08-29 09:50:54 -0500

    TXM-13715 Merge TXM-13637 (#2454)
    TXM-13637 G2S Pages not using touch keyboard (#2404)
    https://jerry.aristocrat.com/browse/TXM-13715

--- Gaming/Aristocrat.Monaco.Gaming.UI/Views/ButtonDeck/VbdViewTemplateSelector.cs
+++ Gaming/Aristocrat.Monaco.Gaming.UI/Views/ButtonDeck/VbdViewTemplateSelector.cs
@@ -1,8 +1,10 @@
 ﻿namespace Aristocrat.Monaco.Gaming.UI.Views.ButtonDeck
 {
+    using System.Reflection;
     using System.Windows;
     using System.Windows.Controls;
     using Contracts.ButtonDeck;
+    using log4net;
     using ViewModels;
 
     /// <summary>
@@ -10,6 +12,8 @@
     /// </summary>
     public class VbdViewTemplateSelector : DataTemplateSelector
     {
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         /// <inheritdoc />
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
@@ -21,6 +25,8 @@
             var element = container as FrameworkElement;
 
             var templateKey = $"Vbd{VirtualButtonDeckHelper.GetVbdPrefixNameByCabinetType()}Template";
+
+            Logger.Debug($"Vbd View Template Selector: {templateKey}");
 
             return element?.TryFindResource(templateKey) as DataTemplate;
         }
