dd4aa5dc5a658d11ac48bb049b938a9489563ca8
Christina <christina.kochan@aristocrat.com>
2023-09-05 13:33:22 -0500

    TXM-13781 Permit RG time limit dialog state while game loading on recovery (#2548)
    https://jerry.aristocrat.com/browse/TXM-13781

--- Gaming/Aristocrat.Monaco.Gaming.UI/LobbyStateManager.cs
+++ Gaming/Aristocrat.Monaco.Gaming.UI/LobbyStateManager.cs
@@ -10,8 +10,8 @@ namespace Aristocrat.Monaco.Gaming.UI
     using Contracts.Models;
     using Hardware.Contracts.Bell;
     using Kernel;
-    using Models;
     using log4net;
+    using Models;
     using Stateless;
 
     /// <summary>
@@ -407,6 +407,7 @@ namespace Aristocrat.Monaco.Gaming.UI
 
             _state.Configure(LobbyState.GameLoading)
                 .OnEntryFrom(_launchGameTrigger, CallStateEntry)
+                .InternalTransition(LobbyTrigger.ResponsibleGamingTimeLimitDialog, ResponsibleGamingWhileGameLoading)
                 .Permit(LobbyTrigger.GameLoaded, LobbyState.Game)
                 .PermitIf(LobbyTrigger.Disable, LobbyState.Disabled, () => !AllowSingleGameAutoLaunch || (_gameHistory.IsRecoveryNeeded && IsLoadingGameForRecovery))
                 .PermitDynamic(
@@ -415,7 +416,8 @@ namespace Aristocrat.Monaco.Gaming.UI
                 .PermitDynamic(
                     LobbyTrigger.GameUnexpectedExit,
                     () => IsLoadingGameForRecovery ? LobbyState.Recovery : GetDefaultChooserState())
-                .Permit(LobbyTrigger.InitiateRecovery, LobbyState.Recovery);
+                .Permit(LobbyTrigger.InitiateRecovery, LobbyState.Recovery)
+                .PermitReentryIf(LobbyTrigger.ResponsibleGamingTimeLimitDialogDismissed, () => ContainsAnyState(LobbyState.ResponsibleGamingTimeLimitDialog));
 
             _state.Configure(LobbyState.GameLoadingForDiagnostics)
                 .SubstateOf(LobbyState.Disabled)
@@ -431,7 +433,8 @@ namespace Aristocrat.Monaco.Gaming.UI
                 .Permit(LobbyTrigger.InitiateRecovery, LobbyState.Recovery)
                 .PermitIf(LobbyTrigger.AttractTimer, LobbyState.Attract, () => AllowSingleGameAutoLaunch && _bank.QueryBalance() == 0)
                 .Permit(LobbyTrigger.ResponsibleGamingTimeLimitDialog, LobbyState.ResponsibleGamingTimeLimitDialog)
-                .PermitIf(LobbyTrigger.ReturnToLobby, LobbyState.Chooser, () => !AllowSingleGameAutoLaunch);
+                .PermitIf(LobbyTrigger.ReturnToLobby, LobbyState.Chooser, () => !AllowSingleGameAutoLaunch)
+                .PermitReentryIf(LobbyTrigger.ResponsibleGamingTimeLimitDialogDismissed, () => ContainsAnyState(LobbyState.ResponsibleGamingTimeLimitDialog));
 
             _state.Configure(LobbyState.GameDiagnostics)
                 .SubstateOf(LobbyState.Disabled)
@@ -697,6 +700,12 @@ namespace Aristocrat.Monaco.Gaming.UI
                 _ageWarningOnEnable = true;
             }
             CallStateExit(LobbyState.CashIn);
+        }
+
+        private void ResponsibleGamingWhileGameLoading()
+        {
+            Logger.Debug("Responsible Gaming Triggered While Game Loading.");
+            _lobbyStateQueue.AddStackableState(LobbyState.ResponsibleGamingTimeLimitDialog);
         }
 
         private void ResponsibleGamingWhileDisabled()
