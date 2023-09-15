f752216f7f775e6c4b3fd7d3eef29bfe63408466
Chris Tisdale <christopher.tisdale@aristocrat.com>
2023-09-14 19:16:11 -0500

    TXARCH-1490 Fixing issues with multiple versions of packages (#2669)

--- Hardware/Aristocrat.Monaco.Hardware.Serial/Printer/SerialPrinterExtensions.cs
+++ Hardware/Aristocrat.Monaco.Hardware.Serial/Printer/SerialPrinterExtensions.cs
@@ -4,7 +4,6 @@
 
     public static class SerialPrinterExtensions
     {
-
         static SerialPrinterExtensions()
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
