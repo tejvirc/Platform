28c078a2fac89aaab40ceb5e843dcb906e90ec96
Art Flachsenhar <137105616+Art-Aristocrat@users.noreply.github.com>
2023-09-20 14:05:05 -0700

    TXARCH - 1167 Implemented Analytics Profile Command Builder (#2709)
    Created the AnalyitcsProfileCommandBuilder

---
+++ Protocol/G2S/Aristocrat.Monaco.G2S/Handlers/Analytics/AnalyticsProfileCommandBuilder.cs
@@ -0,0 +1,37 @@
+﻿namespace Aristocrat.Monaco.G2S.Handlers.Analytics
+{
+    using System;
+    using System.Threading.Tasks;
+    using Aristocrat.G2S.Client.Devices;
+    using Aristocrat.G2S.Protocol.v21;
+
+    /// <summary>
+    ///     An implementation of <see cref="ICommandBuilder{IAnalyticsDevice, analyticsProfile}" />
+    /// </summary>
+    public class AnalyticsProfileCommandBuilder : ICommandBuilder<IAnalyticsDevice, analyticsProfile>
+    {
+        public Task Build(IAnalyticsDevice device, analyticsProfile command)
+        {
+            if (device == null)
+            {
+                throw new ArgumentNullException(nameof(device));
+            }
+
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            command.configurationId = device.ConfigurationId;
+            command.restartStatus = device.RestartStatus;
+            command.useDefaultConfig = device.UseDefaultConfig;
+            command.requiredForPlay = device.RequiredForPlay;
+            command.timeToLive = device.TimeToLive;
+            command.noResponseTimer = device.NoResponseTimer;
+            command.noMessageTimer = device.NoMessageTimer;
+            command.noHostText = device.NoHostText;
+
+            return Task.CompletedTask;
+        }
+    }
+}
