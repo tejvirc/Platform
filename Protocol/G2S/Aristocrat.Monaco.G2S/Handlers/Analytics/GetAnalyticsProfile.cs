709dc3c00990c68b3f6ae3f04d7777869d333e92
Art Flachsenhar <137105616+Art-Aristocrat@users.noreply.github.com>
2023-09-20 15:07:47 -0700

    TXARCH - 1166 Implement get analytics status and profile (#2711)

---
+++ Protocol/G2S/Aristocrat.Monaco.G2S/Handlers/Analytics/GetAnalyticsProfile.cs
@@ -0,0 +1,56 @@
+﻿namespace Aristocrat.Monaco.G2S.Handlers.Analytics
+{
+    using System;
+    using System.Threading.Tasks;
+    using Aristocrat.G2S.Client.Devices;
+    using Aristocrat.G2S.Client;
+    using Aristocrat.G2S;
+    using Aristocrat.G2S.Protocol.v21;
+
+    /// <summary>
+    ///     Handles the v21.getAnalyticsProfile G2S message
+    /// </summary>
+    public class GetAnalyticsProfile : ICommandHandler<analytics, getAnalyticsProfile>
+    {
+        private readonly IG2SEgm _egm;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GetAnalyticsProfile" /> class.
+        ///     Creates a new instance of the GetAnalyticsProfile handler
+        /// </summary>
+        /// <param name="egm">A G2S egm</param>
+        public GetAnalyticsProfile(IG2SEgm egm)
+        {
+            _egm = egm ?? throw new ArgumentNullException(nameof(egm));
+        }
+
+        /// <inheritdoc />
+        public async Task<Error> Verify(ClassCommand<analytics, getAnalyticsProfile> command)
+        {
+            return await Sanction.OwnerAndGuests<IAnalyticsDevice>(_egm, command);
+        }
+
+        /// <inheritdoc />
+        public Task Handle(ClassCommand<analytics, getAnalyticsProfile> command)
+        {
+            var device = _egm.GetDevice<IAnalyticsDevice>(command.IClass.deviceId);
+            if (device == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            var response = command.GenerateResponse<analyticsProfile>();
+            var profile = response.Command;
+
+            profile.configurationId = device.ConfigurationId;
+            profile.restartStatus = device.RestartStatus;
+            profile.requiredForPlay = device.RequiredForPlay;
+            profile.timeToLive = device.TimeToLive;
+            profile.noResponseTimer = device.NoResponseTimer;
+            profile.noMessageTimer = device.NoMessageTimer;
+            profile.noHostText = device.NoHostText;
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Protocol/G2S/Aristocrat.Monaco.G2S/Handlers/Analytics/GetAnalyticsStatus.cs
@@ -0,0 +1,50 @@
+﻿namespace Aristocrat.Monaco.G2S.Handlers.Analytics
+{
+    using System;
+    using System.Threading.Tasks;
+    using Aristocrat.G2S;
+    using Aristocrat.G2S.Client;
+    using Aristocrat.G2S.Client.Devices;
+    using Aristocrat.G2S.Protocol.v21;
+
+    /// <summary>
+    ///     Handles the v21.getAnalyticsStatus G2S message
+    /// </summary>
+    public class GetAnalyticsStatus : ICommandHandler<analytics, getAnalyticsStatus>
+    {
+        private readonly IG2SEgm _egm;
+        private readonly ICommandBuilder<IAnalyticsDevice, analyticsStatus> _command;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GetAnalyticsStatus" /> class.
+        ///     Creates a new instance of the GetAnalyticsStatus handler
+        /// </summary>
+        /// <param name="egm">A G2S egm</param>
+        /// <param name="command">Command builder.</param>
+        public GetAnalyticsStatus(IG2SEgm egm, ICommandBuilder<IAnalyticsDevice, analyticsStatus> command)
+        {
+            _egm = egm ?? throw new ArgumentNullException(nameof(egm));
+            _command = command ?? throw new ArgumentNullException(nameof(command));
+        }
+
+        /// <inheritdoc />
+        public async Task<Error> Verify(ClassCommand<analytics, getAnalyticsStatus> command)
+        {
+            return await Sanction.OwnerAndGuests<IAnalyticsDevice>(_egm, command);
+        }
+
+        /// <inheritdoc />
+        public async Task Handle(ClassCommand<analytics, getAnalyticsStatus> command)
+        {
+            var device = _egm.GetDevice<IAnalyticsDevice>(command.IClass.deviceId);
+            if (device == null)
+            {
+                return;
+            }
+
+            var response = command.GenerateResponse<analyticsStatus>();
+            var status = response.Command;
+            await _command.Build(device, status);
+        }
+    }
+}
