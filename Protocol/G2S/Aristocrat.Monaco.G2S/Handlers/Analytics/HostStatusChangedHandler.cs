36d1999ed0e6cfd7e751612e27ec40c578f586cb
Art Flachsenhar <137105616+Art-Aristocrat@users.noreply.github.com>
2023-09-21 09:02:46 -0700

    TXARCH - 1164 Added HostStatusChangedHandler to Analytics (#2712)

---
+++ Protocol/G2S/Aristocrat.Monaco.G2S/Handlers/Analytics/HostStatusChangedHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace Aristocrat.Monaco.G2S.Handlers.Analytics
+{
+    using System;
+    using Aristocrat.G2S;
+    using Aristocrat.G2S.Client;
+    using Aristocrat.G2S.Client.Devices;
+    using Aristocrat.G2S.Client.Devices.v21;
+    using Aristocrat.G2S.Protocol.v21;
+
+    public class HostStatusChangedHandler : IStatusChangedHandler<IAnalyticsDevice>
+    {
+        private readonly ICommandBuilder<IAnalyticsDevice, analyticsStatus> _commandBuilder;
+        private readonly IEventLift _eventLift;
+
+        public HostStatusChangedHandler(
+            ICommandBuilder<IAnalyticsDevice, analyticsStatus> commandBuilder,
+            IEventLift eventLift)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+            _eventLift = eventLift ?? throw new ArgumentNullException(nameof(eventLift));
+        }
+
+        public void Handle(IAnalyticsDevice device)
+        {
+            var status = new analyticsStatus();
+            _commandBuilder.Build(device, status);
+            _eventLift.Report(
+                device,
+                device.HostEnabled ? EventCode.ATI_ANE004 : EventCode.ATI_ANE003,
+                device.DeviceList(status));
+        }
+    }
+}
